b204849c9a7337e78a1535f8e23b5e3a3bcc1c1f
toriwat328 <toriwat328@gmail.com>
2019-10-19 21:08:44 -0400

    functionality updates

--- where_in_the_world/app.js
+++ where_in_the_world/app.js
@@ -2,7 +2,7 @@ const countries = ['ICELAND', 'MOROCCO', 'AUSTRIA']; //made an array of names of
 
 let round = 1; //Game starts on round 1
 
-let triesNum = 0; //Each round, a user has 3 tries
+let triesNum = 3; //Each round, a user has 3 tries
 
 let i = 0;
 
@@ -23,18 +23,30 @@ $('#openInstructions').on('click', () => { //event handeler where on click the i
 
 $('form').on('submit', (event) => { // when user submits form,
     event.preventDefault() //prevent default reload after submit
+    triesNum--;
 
-    triesNum++
+
+
+
+
+
+
 
     $('form').addClass('clicked');
 
-    const userInput = $('input[type="text"]').val().toUpperCase(); // text submitted is turned uppercase and saved in variable
+    // if(userInput !== countries[i] && triesNum === 0) {
+    //     var userInput = countries[i]
+    //
+    // } else {
+        const userInput = $('input[type="text"]').val().toUpperCase(); // text submitted is turned uppercase and saved in variable
+    // }
 
 
+var url = 'http://api.weatherstack.com/current?access_key=987e40f5d0c5db5e98ec4246d66b6cd3&query=' + userInput
 
 
 $.ajax({
-    url: 'http://api.weatherstack.com/current?access_key=987e40f5d0c5db5e98ec4246d66b6cd3&query=' + userInput //using AJAX to connect to weatherstack API. userInput is added to URL in order to change information displayed on the site per round
+    url: url //using AJAX to connect to weatherstack API. userInput is added to URL in order to change information displayed on the site per round
 
 }).then(
     (data) => {
@@ -79,18 +91,80 @@ $.ajax({
                 $('div#weather-win').children().eq(4).attr('id', 'weather-icon')
 
                 $('#win-modal').show(500); // modal for when user wins will pop up automatically
-                    $('.modal-buttons').on('click', () => { //when user clicks close button
+                    $('#close1').on('click', () => { //when user clicks close button
                         $('#win-modal').hide(400); // the win modal will hide
+                        if(round === 3) {
+                            $('.you-won-modal').show(500);
+                            $('#close4').on('click', () => {
+                                $('.you-won-modal').show(400);
+                            })
+                        }
                         $('main').append('<button>Next Round</button>');
                         $('button').attr('id', 'next_button'); // once the modal is closed a button is made to go to next round
-                            $('button').on('click', (event) => { // on click of next round button, the images shown will change to the next country imags in array
+                            $('button#next_button').on('click', (event) => { // on click of next round button, the images shown will change to the next country imags in array
                                 i++ //increase index in array
 
                                 $('.pic1').css('background-image', 'url("imgs/' + countries[i] +'1.JPG")');
                                 $('.pic2').css('background-image', 'url("imgs/' + countries[i] +'2.JPG")');
                                 $('.pic3').css('background-image', 'url("imgs/' + countries[i] +'3.JPG")');
                                 round++ //when next button is clicked user goes to next round
-                                triesNum = 0
+                                triesNum = 3
+                                $('form').removeClass('clicked');
+                                $(event.currentTarget).remove();
+                                $(this).remove();
+
+
+
+                        });
+
+                });
+            } else if (userInput !== countries[i] && triesNum === 0) {
+
+                url = 'http://api.weatherstack.com/current?access_key=987e40f5d0c5db5e98ec4246d66b6cd3&query=' + countries[i]
+                console.log(url)
+
+                $('div#status-force').remove();
+                $('div#weather-force').remove();
+                const $div4 = $('<div>').attr('id', 'status-force').appendTo('#div-tobe-empty2');
+                $('iframe').attr('src', "https://www.google.com/maps/embed/v1/place?key=AIzaSyAsnO8_jZ89RFL-22mqlWwVkJsMTYtRRzY&q='" + userInput);
+                const $div5 = $('<div>').attr('id','weather-force').appendTo('#div-tobe-empty2');
+
+
+                $('#status-force').append('<p>Your In ' + countries[i] + '. But I forgive you. Go to the next round!</p>'); //will need to delete everything in modal when next round begins so need to add empty method and add all modal contents here.
+
+                $('div#weather-force').append('<p>' + data.location.name + '</p>') // API is used to show weather city name for current country
+                $('div#weather-force').children().eq(0).attr('id', 'cityName')
+
+
+                $('div#weather-force').append('<p>' + data.location.country + '</p>') // API shows country for current country being iterated
+                $('div#weather-force').children().eq(1).attr('id', 'country')
+
+
+                $('div#weather-force').append('<p>' + data.current.temperature + ' Degrees</p>')  //API shows temp for current country being iterated
+                $('div#weather-force').children().eq(2).attr('id', 'temp')
+
+
+                $('div#weather-force').append('<p>' + data.current.weather_descriptions["0"] + '</p>');// API shows weather description
+                $('div#weather-force').children().eq(3).attr('id', 'descrip')
+
+
+                $('div#weather-force').append('<p>' + data.current.weather_icons["0"] + '</p>'); //need to make weather icon a img attr
+                $('div#weather-force').children().eq(4).attr('id', 'weather-icon')
+
+                $('#modal-3-tries').show(500); // modal for when user wins will pop up automatically
+                    $('.#close2').on('click', () => { //when user clicks close button
+                        $('#modal-3-tries').hide(400); // the win modal will hide
+
+                        $('main').append('<button>Next Round</button>');
+                        $('button').attr('id', 'next_button'); // once the modal is closed a button is made to go to next round
+                            $('button#next_button').on('click', (event) => { // on click of next round button, the images shown will change to the next country imags in array
+                                i++ //increase index in array
+
+                                $('.pic1').css('background-image', 'url("imgs/' + countries[i] +'1.JPG")');
+                                $('.pic2').css('background-image', 'url("imgs/' + countries[i] +'2.JPG")');
+                                $('.pic3').css('background-image', 'url("imgs/' + countries[i] +'3.JPG")');
+                                round++ //when next button is clicked user goes to next round
+                                triesNum = 3
                                 $('form').removeClass('clicked');
                                 $(event.currentTarget).remove();
                                 $(this).remove();
@@ -98,77 +172,31 @@ $.ajax({
                                 // if in round 2, restart process of form submit with 3rd element in countries array
 
 
-                        })
+
 
                 });
-            } else if (userInput !== countries[i] && triesNum >= 3) {
-                        userInput = countries[i]
-
-                        $('div#status-force').remove();
-                        $('div#weather-force').remove();
-                        const $div4 = $('<div>').attr('id', 'status-force').appendTo('#div-tobe-empty2');
-                        $('iframe').attr('src', "https://www.google.com/maps/embed/v1/place?key=AIzaSyAsnO8_jZ89RFL-22mqlWwVkJsMTYtRRzY&q='" + userInput);
-                        const $div5 = $('<div>').attr('id','weather-force').appendTo('#div-tobe-empty2');
 
 
+                    });
 
-                        $('#status-force').append('<p>Your In ' + countries[i] + '. But I forgive you. Go to the next round!</p>'); //will need to delete everything in modal when next round begins so need to add empty method and add all modal contents here.
+                } else if (userInput !== countries[i]) { //After form is submitted, if userInput is not equal to current country being iterated, they are incorrect and the following will happen...
 
-                        $$('div#weather-force').append('<p>' + data.location.name + '</p>') // API is used to show weather city name for current country
-                        $('div#weather-force').children().eq(0).attr('id', 'cityName')
-
-
-                        $('div#weather-force').append('<p>' + data.location.country + '</p>') // API shows country for current country being iterated
-                        $('div#weather-force').children().eq(1).attr('id', 'country')
+                            // How do I make sure that correct lowercase country name is considers correct?
 
 
-                        $('div#weather-force').append('<p>' + data.current.temperature + ' Degrees</p>')  //API shows temp for current country being iterated
-                        $('div#weather-force').children().eq(2).attr('id', 'temp')
+                            $('div#status-wrong').remove();
+                            const $div3 = $('<div>').attr('id', 'status-wrong').appendTo('#modal-textbox-wrong');
 
 
-                        $('div#weather-force').append('<p>' + data.current.weather_descriptions["0"] + '</p>');// API shows weather description
-                        $('div#weather-force').children().eq(3).attr('id', 'descrip')
+                            $('div#status-wrong').append('<p>Try Again.</br> Tries Left: ' + triesNum + '</p>'); // append p tag to modal text box telling user to try again
+                            $('#modal-wrong').show(500); // modal for when user submits incorrect answer and its not that 3rd try, will pop up automatically
+                            $('#close3').on('click', () => { // when user clicks continue button
+                                $('#modal-wrong').hide(400); //the wrong modal will hide, and user will have a change to submit form again
+                                    $('form').removeClass('clicked');
 
+                                        })
 
-                        $('div#weather-force').append('<p>' + data.current.weather_icons["0"] + '</p>'); //need to make weather icon a img attr
-                        $('div#weather-force').children().eq(4).attr('id', 'weather-icon')
-
-                        $('#modal-3-tries').show(500); // modal for when user wins will pop up automatically
-                            $('.modal-buttons').on('click', () => { //when user clicks close button
-                                $('#modal-3-tries').hide(400); // the win modal will hide
-                                $('main').append('<button>NextRound</button>').attr('id', 'next_button'); // once the modal is closed a button is made to go to next round
-                                    $('#next_button').on('click', () => { // on click of next round button, the images shown will change to the next country imags in array
-                                        $('.pic1').css('background-image', 'url("imgs/' + countries[i+1] +'1.JPG")');
-                                        $('.pic2').css('background-image', 'url("imgs/' + countries[i+1] +'2.JPG")');
-                                        $('.pic3').css('background-image', 'url("imgs/' + countries[i+1] +'3.JPG")');
-                                        round++ //when next button is clicked round number is increase and user goes to next round
-                                        i++
-                                        triesNum = 0 //reset tries to 0 for next round start
-                                        $('#next_button').remove();
-                                        // if in round 1, restart process of form submit with 2nd element in countries array
-                                        // if in round 2, restart process of form submit with 3rd element in countries array
-                                        $('form').removeClass('clicked');
-
-
-                    })
-            });
-
-            } else if (userInput !== countries[i]) { //After form is submitted, if userInput is not equal to current country being iterated, they are incorrect and the following will happen...
-
-            // How do I make sure that correct lowercase country name is considers correct?
-
-            $('div#status-wrong').remove();
-            const $div3 = $('<div>').attr('id', 'status-wrong').appendTo('#modal-textbox-wrong');
-
-
-            $('div#status-wrong').append('<p>Try Again</p>'); // append p tag to modal text box telling user to try again
-            $('#modal-wrong').show(500); // modal for when user submits incorrect answer and its not that 3rd try, will pop up automatically
-            $('.modal-buttons').on('click', () => { // when user clicks continue button
-                $('#modal-wrong').hide(400); //the wrong modal will hide, and user will have a change to submit form again
-                    $('form').removeClass('clicked');
-                        })
-
-                }
+                                }
 
 
         }else {
